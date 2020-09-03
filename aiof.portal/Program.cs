66adb4a771d6932e6ec4261e48edfb708cde3f0f
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-03 13:31:24 -0400

    add login page

--- aiof.portal/Program.cs
+++ aiof.portal/Program.cs
@@ -19,8 +19,11 @@ namespace aiof.portal
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddLogging();
 
-            await builder.Build().RunAsync();
+            await builder
+                .Build()
+                .RunAsync();
         }
     }
 }
