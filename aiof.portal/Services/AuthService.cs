a8c6dfd7820ea2691a71d6afbf7d3f27b6b31eb5
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-03 20:13:20 -0400

    update auth service

--- aiof.portal/Services/AuthService.cs
+++ aiof.portal/Services/AuthService.cs
@@ -1,15 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace aiof.portal.Services
 {
     public class AuthService : IAuthService
     {
-        public ILocalStorageService _localStorageService;
+        public readonly IAuthClient _client;
+        public readonly ILocalStorageService _localStorageService;
 
         public AuthService(
+            IAuthClient client,
             ILocalStorageService localStorageService)
         {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
             _localStorageService = localStorageService ?? throw new ArgumentNullException(nameof(localStorageService));
+        }
+
+        public async Task<HttpResponseMessage> LoginAsync(
+            string username,
+            string password)
+        {
+            return await _client.LoginAsync(username, password);
         }
     }
 }
