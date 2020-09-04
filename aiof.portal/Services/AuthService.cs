351864925b77ded9434bbdcd75555348d03491c2
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-03 22:17:53 -0400

    update auth service login

--- aiof.portal/Services/AuthService.cs
+++ aiof.portal/Services/AuthService.cs
@@ -8,6 +8,8 @@ using System.Net.Http;
 
 using Microsoft.AspNetCore.Components;
 
+using Microsoft.AspNetCore.Components.Authorization;
+
 using Blazored.LocalStorage;
 
 using aiof.portal.Models;
@@ -18,15 +20,18 @@ namespace aiof.portal.Services
     {
         public readonly IAuthClient _client;
         public readonly ILocalStorageService _localStorageService;
+        public readonly AuthenticationStateProvider _authenticationStateProvider;
         public readonly NavigationManager _navigationManager;
 
         public AuthService(
             IAuthClient client,
             ILocalStorageService localStorageService,
+            AuthenticationStateProvider authenticationStateProvider,
             NavigationManager navigationManager)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
             _localStorageService = localStorageService ?? throw new ArgumentNullException(nameof(localStorageService));
+            _authenticationStateProvider = authenticationStateProvider ?? throw new ArgumentNullException(nameof(authenticationStateProvider));
             _navigationManager = navigationManager ?? throw new ArgumentNullException(nameof(navigationManager));
         }
 
@@ -39,7 +44,9 @@ namespace aiof.portal.Services
             if(resp.IsSuccessStatusCode)
             {
                 var user = JsonSerializer.Deserialize<User>(await resp.Content.ReadAsByteArrayAsync());
-                await _localStorageService.SetItemAsync(Keys.User, user);
+                await _localStorageService.SetItemAsync(Keys.User, user);                
+                await _authenticationStateProvider.GetAuthenticationStateAsync();
+                _navigationManager.NavigateTo("/");
             }
         }
 
