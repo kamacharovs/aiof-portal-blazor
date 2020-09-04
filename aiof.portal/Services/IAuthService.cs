2eab766777c00c6f8df6920604a6b1b0f7ccfc4c
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-03 20:21:00 -0400

    update to fix issues

--- aiof.portal/Services/IAuthService.cs
+++ aiof.portal/Services/IAuthService.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace aiof.portal.Services
 {
     public interface IAuthService
     {
-        
+        Task<HttpResponseMessage> LoginAsync(
+            string username,
+            string password);
     }
 }
