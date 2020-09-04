bc03da2ae7448bbc762d8bcfde0de6c348e64ff9
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-03 20:59:01 -0400

    update to set default cookie

--- aiof.portal/Services/ICookieStorageService.cs
+++ aiof.portal/Services/ICookieStorageService.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Threading.Tasks;
 
 namespace aiof.portal.Services
 {
     public interface ICookieStorageService
     {
-        
+        Task<T> SetAsync<T>(
+            string key,
+            DateTime expires,
+            string path = "/");
     }
 }
