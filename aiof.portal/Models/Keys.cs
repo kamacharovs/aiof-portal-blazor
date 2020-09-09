e6c687232070ceda0a26117c6f4c89330efb6eb7
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-09 13:17:05 -0400

    update

--- aiof.portal/Models/Keys.cs
+++ aiof.portal/Models/Keys.cs
@@ -4,6 +4,7 @@ namespace aiof.portal.Models
 {
     public static class Keys
     {
+        public static string Auth = nameof(Auth);
         public static string User = nameof(User);
 
         public static string AccessToken = "access_token";
